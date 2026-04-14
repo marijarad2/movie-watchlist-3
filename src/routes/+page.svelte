3c66acb76cf36af64378e4dd0494868c1a51603a
marijarad2 <marijaradovanovic029@gmail.com>
2026-04-14 18:53:09 +0200

    update cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
 <h1>Welcome to ScreenStack</h1>
-<p>Your tracker and search engine for movies and tv shows</p>
+<script>
+  import MovieCard from "$lib/components/MovieCard.svelte";
+
+  let { data } = $props();
+  let movies = data.movies;
+</script>
+
+<p><i>Daten und Bilder generiert mit ChatGPT und DALL-E</i></p>
+
+<div class="row">
+  {#each movies as movie}
+    <div class="col-sm-6 col-md-4 col-lg-3 mb-2 gx-2">
+      <MovieCard movie={movie}></MovieCard>
+    </div>
+  {/each}
+</div>
